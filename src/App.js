9026e90a5ea7ac156de5b94538ae430a06363a00
Jason Andrada <jpa323@nyu.edu>
2018-02-21 12:53:34 -0500

    add turn count functionality. TODO: find a way to get rid of users and pass turn based on onlineUsers

--- src/App.js
+++ src/App.js
@@ -1,34 +1,13 @@
 import React, { Component } from 'react';
 import io from 'socket.io-client';
 const socket = io();
-
+import Tracker from './Tracker';
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      one: 'two',
-      three: 'four',
-      five: 'six'
-    }
-  }
-
-  componentDidMount() {
-    socket.emit('enter', {
-      user: 'test user'
-    })
-  }
-
-  passTurn() {
-    socket.emit('change turn');
-  }
-
-  render() {
-    //TODO: add logic that will eventually move the game forward.
-    console.log('loaded', this.state)
-    return (
+ render() {
+   return (
       <div className="App">
-        <h1>Eventually this will turn into a sockets project</h1>
-        <button onClick={this.passTurn}>Pass your turn</button>
+        <h1>Oh hey, this is a sockets project</h1>
+        <Tracker />
       </div>
     )
   }
