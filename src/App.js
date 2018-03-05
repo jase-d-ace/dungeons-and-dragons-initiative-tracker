c823873b78b997932716c12849f8dfa95551627b
Jason Andrada <jpa323@nyu.edu>
2018-03-05 14:51:56 -0500

    add front end admin logic

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Tracker from './components/Tracker';
 import Login from './components/Login';
 import Register from './components/Register'
+import Admin from './components/Admin';
+import AdminRegister from './components/AdminRegister';
+import AdminTracker from './components/AdminTracker';
 
 class App extends Component {
  render() {
@@ -12,6 +15,9 @@ class App extends Component {
         <Route exact path="/" component={Login} />
         <Route path="/tracker" component={Tracker} />
         <Route path="/signup" component={Register} />
+        <Route exact path='/admin' component={Admin} />
+        <Route path='/admin/register' component={AdminRegister} />
+        <Route path='/admin/tracker' component={AdminTracker} />
       </div>
      </BrowserRouter>
     )
---
+++ src/components/AdminTracker.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class AdminTracker extends Component {
+  render() {
+    return(
+      <h1>Admin path is live!</h1>
+    )
+  }
+}
+
+export default AdminTracker
