7a3d2e7133bf4fce883ff1e5e7b6cc6c91a58f2a
Jason Andrada <jpa323@nyu.edu>
2018-03-05 14:50:03 -0500

    add admin auth logic

---
+++ services/admin/local-admin.js
@@ -0,0 +1,36 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const init = require('./passport-admin');
+const adminModel = require('../../models/admin');
+const Admin = new adminModel;
+const authClass = require('../auth-helpers');
+const AuthHelpers = new authClass;
+
+const options = {
+  usernameField: 'name',
+  passwordField: 'password'
+};
+
+init();
+
+passport.use('admin',
+  new LocalStrategy(options, (name, password, done) => {
+    Admin.findByName(name)
+    .then( admin => {
+      if (!admin) {
+        return done(null, false);
+      };
+      if (!AuthHelpers.comparePass(password, admin.password_digest)) {
+        return done(null, false);
+      } else {
+        return done(null, admin);
+      };
+    })
+    .catch( err => {
+      console.log('messed up in local-admin');
+      return done(err)
+    });
+  })
+);
+
+module.exports = passport
---
+++ services/admin/passport-admin.js
@@ -0,0 +1,19 @@
+const passport = require('passport');
+const adminClass = require('../models/admin');
+const Admin = new adminClass;
+
+module.exports = () => {
+  passport.serializeUser( (user, done) => {
+    done(null, user.name)
+  });
+
+  passport.deserializeUser( (name, done) => {
+    Admin.findByName(name)
+    .then( admin => {
+      done(null, admin);
+    })
+    .catch( err => {
+      done(err, null);
+    });
+  });
+};
