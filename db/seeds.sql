6bf2b80aa90f1b5c9e1fcfcc80e351cdf5bcf7d5
Jason Andrada <jpa323@nyu.edu>
2018-03-10 11:26:44 -0500

    uncomment one more line of seed data

--- db/seeds.sql
+++ db/seeds.sql
@@ -32,7 +32,7 @@ CREATE TABLE IF NOT EXISTS admins(
   password_digest VARCHAR(255) NOT NULL
   )
 
--- INSERT INTO characters(name, race, class, ac, health, str_mod, dex_mod, con_mod, int_mod, wis_mod, cha_mod, proficiency, user_id) VALUES
+INSERT INTO characters(name, race, class, ac, health, str_mod, dex_mod, con_mod, int_mod, wis_mod, cha_mod, proficiency, user_id) VALUES
 ('sylvan', 'wood elf', 'monk', '16', '45', '+0', '+4', '+1', '+1', '+2', '-1', '+3', 1)
 ('ranathar', 'drow', 'rogue', '14', '32', '+1', '+4', '+0', '+1', '-1', '+2', '+3', 2)
 ('algol', 'wood elf', 'barbarian', '14', '68', '+3', '+2', '+2', '-1', '+1', '+0', '+3', 3)
