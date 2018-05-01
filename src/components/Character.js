a571c35e0735547372cf24dc40a0d4fcb3392f68
Jason Andrada <jpa323@nyu.edu>
2018-05-01 13:10:53 -0400

    add styles to character

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import '../styles/character.css';
 
 const Character = (props) => {
   return (
     <div className="Character">
-      <h1>{props.name}, the {props.race} {props.class}</h1>
-      <p>Proficiency: {props.proficiency}</p>
-      <p>Str Mod: {props.str_mod}</p>
-      <p>Dex Mod: {props.dex_mod}</p>
-      <p>Con Mod: {props.con_mod}</p>
-      <p>Int Mod: {props.int_mod}</p>
-      <p>Cha Mod: {props.cha_mod}</p>
+      <div className='character-container'>
+        <h1 className='character-header'>{props.name}, the {props.race} {props.class}</h1>
+        <div className='stats'>
+          <p>Proficiency: {props.proficiency}</p>
+          <p>Str Mod: {props.str_mod}</p>
+          <p>Dex Mod: {props.dex_mod}</p>
+          <p>Con Mod: {props.con_mod}</p>
+          <p>Int Mod: {props.int_mod}</p>
+          <p>Cha Mod: {props.cha_mod}</p>
+        </div>
+      </div>
     </div>
   )
 }
